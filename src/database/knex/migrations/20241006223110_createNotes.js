f061081be5e47ef556e10abc99c4a42ec5d85747
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2024-10-06 19:37:07 -0300

    Create 20241006223110_createNotes.js

---
+++ src/database/knex/migrations/20241006223110_createNotes.js
@@ -0,0 +1,15 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("notes", (table) => {
+    table.increments("id");
+    table.text("title");
+    table.text("description");
+    table.text("avaliation");
+    table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
+    table.timestamp("created_at").default(knex.fn.now());
+    table.timestamp("updated_at").default(knex.fn.now());
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("notes");
+};
